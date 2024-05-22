544d1043c39c6a619c157b467edd143ed445a1f1
ViSiOnOp19cr <chandancr98195@email.com>
2024-05-22 01:32:21 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name="diabetes",
+    version="0.0.0",
+    author="vision",
+    author_email="chandancr98195@gmail.com",
+    packages=find_packages()
+)
