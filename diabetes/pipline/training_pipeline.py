3c1e2fe5a9fec2ecc5010e2c5115a8efa4a3ba1d
ViSiOnOp19cr <chandancr98195@email.com>
2024-05-22 04:17:40 -0700

    data_validation

--- diabetes/pipline/training_pipeline.py
+++ diabetes/pipline/training_pipeline.py
@@ -3,13 +3,16 @@ from diabetes.exception import diabetesException
 from diabetes.logger import logging
 
 from diabetes.components.data_ingestion import DataIngestion
+from diabetes.components.data_validation import DataValidation
 
 
 from diabetes.entity.config_entity import (DataIngestionConfig,
+                                           DataValidationConfig
                                           )
                                           
 
 from diabetes.entity.artifact_entity import (DataIngestionArtifact,
+                                             DataValidationArtifact
                                             )
 
 
@@ -17,6 +20,7 @@ from diabetes.entity.artifact_entity import (DataIngestionArtifact,
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
         
 
 
@@ -35,6 +39,31 @@ class TrainPipeline:
                 "Exited the start_data_ingestion method of TrainPipeline class"
             )
             return data_ingestion_artifact
+        except Exception as e:
+            raise diabetesException(e, sys) from e
+        
+
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
         except Exception as e:
             raise diabetesException(e, sys) from e
     def run_pipeline(self, ) -> None:
